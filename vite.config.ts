1618fee1ea210e0b01fc703781ddbeccec30e0e6
Eason Chen <easoncxz@gmail.com>
2025-04-18 23:07:16 +0300

    Try Nix CI

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,8 @@ export default defineConfig({
     port: 8080
   },
   base: process.env.BASE_URL || '/',
+  build: {
+    outDir: path.join(__dirname, "docs"), // match Github Pages defaults
+    emptyOutDir: true, // https://stackoverflow.com/questions/66863200/changing-the-input-and-output-directory-in-vite
+  },
 })
